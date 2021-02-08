67ea1e10c7f5d3f80fe95c6893c7494b6883636f
nshrik <nshrik@gmail.com>
2021-02-07 21:48:56 -0500

    init

---
+++ Content/themes/base/AsyncProcessing1.js
@@ -0,0 +1,60 @@
+﻿// JScript File
+
+//Added By Yogesh Babu For Async Message
+Sys.WebForms.PageRequestManager.getInstance().add_beginRequest(BeginRequestHandler);
+Sys.WebForms.PageRequestManager.getInstance().add_endRequest(EndRequestHandler);
+function BeginRequestHandler(sender, args) {
+    var elem = args.get_postBackElement();
+
+    // DivMain.style.display = 'block';
+    // Adding Dark page
+    DarkenPage();
+    //if (DivMain != null) {
+    //    DivMain.style.width = screen.width - 20;
+    //    DivMain.style.height = screen.height - 150;
+    //}
+    //divdasrk.width = (document.body.scrollHeight) + 'px';
+    //divdasrk.height = $(window).height;//screen.height + 200 + 'px';//(document.body.scrollHeight) + 'px';
+    //divdasrk.style.display = '';
+
+    //ActivateAlertDiv('visible', 'divdasrk', '');
+    ActivateAlertDiv('visible', 'DivProcess', elem.value + ' processing...');
+
+}
+function EndRequestHandler(sender, args) {
+
+    //  DivMain.style.display='none';
+    // ActivateAlertDiv('hidden', 'divdasrk', '');
+    ActivateAlertDiv('hidden', 'DivProcess', '');
+    // Darkpage none
+    LightenPage();
+
+}
+
+function ActivateAlertDiv(visstring, elem, msg) {
+
+
+    var adiv = $get(elem);
+    if (adiv != null) {
+        adiv.style.visibility = visstring;
+    }
+    //adiv.innerHTML = msg;                     
+}
+function DarkenPage() {
+    var page_screen = document.getElementById('page_screen');
+    if (page_screen != null) {
+        page_screen.style.height = (document.body.scrollHeight) + 'px';
+        page_screen.style.width = (document.body.scrollWidth) + 'px';
+        page_screen.style.display = 'block';
+        page_screen.className = 'modalBackground';
+    }
+}
+
+// this function removes the dark screen and the page is light again
+function LightenPage() {
+    var page_screen = document.getElementById('page_screen');
+    if (page_screen != null)
+        page_screen.style.display = 'none';
+    //  divdasrk.style.display = 'none';
+}
+    //End of Add
---
+++ Content/themes/base/showprogressForUpload.js
@@ -0,0 +1,23 @@
+﻿(function ($) {
+
+    $.showprogress = function () {
+        
+        $.hideprogress(); $("BODY").append('<div id="processing_overlay"></div>');
+        $("BODY").append('<div id="processing_container">' + '<div id="processing_content" >' + '<table><tr align="center"><td valign="top"><img src="../Content/themes/base/images/loading.gif" alt="..." style="vertical-align: middle;" /><span>Loading...</span></td></tr></table>' + '</div>' + '</div>');
+        //var pos = ($.browser.msie && parseInt($.browser.version) <= 9) ? 'absolute' : 'fixed';        
+        var pos = 'fixed';
+        $("#processing_container").css({ position: pos, zIndex: 99999, padding: 0, margin: 0 });
+        $("#processing_container").css({ minWidth: $("#processing_container").outerWidth(), maxWidth: $("#processing_container").outerWidth() });
+        var top = (($(window).height() / 2) - ($("#processing_container").outerHeight() / 2)) + (-75);
+        var left = (($(window).width() / 2) - ($("#processing_container").outerWidth() / 2)) + 0; if (top < 0) top = 0;
+        if (left < 0) left = 0;
+        //if ($.browser.msie && parseInt($.browser.version) <= 9) top = top + $(window).scrollTop();
+
+        $("#processing_container").css({ top: top + 'px', left: left + 'px' });
+
+        $("#processing_overlay").height($(document).height());
+    },
+    $.hideprogress = function () {
+        $("#processing_container").remove(); $("#processing_overlay").remove();
+    }, $.showmsg = function (msgEle, msgText, msgClass, msgIcon, msgHideIcon, autoHide) { var tblMsg; tblMsg = '<table width="100%" cellpadding="1" cellspacing="0" border="0" class="' + msgClass + '"><tr><td style="width:30px;" align="center" valign="middle">' + msgIcon + '</td><td>' + msgText + '</td><td style="width:30px;" align="center" valign="middle"><a href="javascript:void(0);" onclick="$(\'#' + msgEle + '\').toggle(400);">' + msgHideIcon + '</a></td></tr></table>'; $("#" + msgEle).html(tblMsg); $("#" + msgEle).show(); if (autoHide) { setTimeout(function () { $('#' + msgEle).fadeOut('normal') }, 10000); } }
+})(jQuery);
